f22808d2f8d1240a7fe6fe64f6fb98bbb5b594f6
Anmol-Rastogi1 <anmolrast9936@gmail.com>
2026-06-02 21:05:35 +0530

    Create Maxsum Array.java

---
+++ Week_1/Day_2/Maxsum Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum =nums[0];
+        int sum=0;
+        int n =nums.length;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxsum=Math.max(sum,maxsum);
+            if(sum<0){
+                sum=0;
+            }
+        }return maxsum;
+    }
+}
