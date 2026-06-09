ecc4121fcddd3510b062967ad84acfdbe3cf1c2f
Anmol-Rastogi1 <anmolrast9936@gmail.com>
2026-06-09 20:53:31 +0530

    Create permutation in string .java

---
+++ Week_2/Day_2/permutation in string .java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
+        }
+        if (matches(s1Count, windowCount)) {
+            return true;
+        }
+        for (int right = s1.length(); right < s2.length(); right++) {
+            windowCount[s2.charAt(right) - 'a']++;
+            int left = right - s1.length();
+            windowCount[s2.charAt(left) - 'a']--;
+            if (matches(s1Count, windowCount)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean matches(int[] arr1, int[] arr2) {
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
