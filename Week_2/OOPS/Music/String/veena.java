e239db00981b29c9cfe631a5b5e9986133eb25cb
Anmol-Rastogi1 <anmolrast9936@gmail.com>
2026-06-13 15:35:39 +0530

    Delete Week_2/OOPS/Music/String directory

--- Week_2/OOPS/Music/String/veena.java
+++
@@ -1,8 +0,0 @@
-package OOPS_Questions_2.music.string;
-import OOPS_Questions_2.music.Playable;
-public class Veena implements Playable {
-    @Override
-    public void play() {
-        System.out.println("Playing Veena");
-    }
-}
