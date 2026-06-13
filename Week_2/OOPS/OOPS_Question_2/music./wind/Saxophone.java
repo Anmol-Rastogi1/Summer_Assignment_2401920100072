aa3739023d7e7965dbbd6354f2dc9b0f5f22205d
Anmol-Rastogi1 <anmolrast9936@gmail.com>
2026-06-13 15:41:48 +0530

    Create Saxophone.java

---
+++ Week_2/OOPS/OOPS_Question_2/music./wind/Saxophone.java
@@ -0,0 +1,8 @@
+package OOPS_Questions_2.music.wind;
+import OOPS_Questions_2.music.Playable;
+public class Saxophone implements Playable {
+    @Override
+    public void play(){
+        System.out.println("Playing Saxophone");
+    }
+}
