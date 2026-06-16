71ffd610f92d23c81e55dc5cc7c049e97734307c
Anmol-Rastogi1 <anmolrast9936@gmail.com>
2026-06-16 22:08:20 +0530

    Create Remove nth node from end of the list.java

---
+++ Week_3/Day_2/Remove nth node from end of the list.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int len = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            len++;
+            curr = curr.next;
+        }
+        if (len == n) return head.next;
+        curr = head;
+        for (int i = 1; i < len - n; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+}
